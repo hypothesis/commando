88f4e1d531e023a3256d0432b41cdb17ebe1de2f
Sean Hammond <seanh@users.noreply.github.com>
2022-10-05 11:35:58 +0100

    Initial implementation

--- tests/unit/commando/core_test.py
+++ tests/unit/commando/core_test.py
@@ -1,6 +1,23 @@
-from commando.core import hello_world
+from unittest.mock import sentinel
+
+import pytest
+
+from commando.core import commando
 
 
-class TestHelloWorld:
+class TestCommando:
     def test_it(self):
-        assert hello_world() == "Hello, world!"
+        exit_status = commando(
+            [sentinel.repo_1, sentinel.repo_2],
+            ["test", "command"],
+            sentinel.commit_message,
+            sentinel.branch,
+            sentinel.pr_title,
+            sentinel.pr_body,
+        )
+
+        assert not exit_status
+
+    @pytest.fixture(autouse=True)
+    def subprocess(self, mocker):
+        return mocker.patch("commando.core.subprocess", autospec=True)
